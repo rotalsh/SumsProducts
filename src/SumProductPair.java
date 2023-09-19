5ce6a45b7f84c228f75b3d1acd1b1d82d41a07f5
rotalsh <100392201+rotalsh@users.noreply.github.com>
2023-09-19 13:46:24 -0700

    Update SumProductPair.java

--- src/SumProductPair.java
+++ src/SumProductPair.java
@@ -2,11 +2,13 @@ public class SumProductPair {
     private int sum;
     private int prod;
 
+    // EFFECTS: makes new SumProductPair with fields sum and prod
     public SumProductPair(int sum, int prod){
         this.sum = sum;
         this.prod = prod;
     }
 
+    // EFFECTS: returns string representation of SumProductPair
     @Override
     public String toString(){
         return "sum: " + sum + "; product: " + prod;
