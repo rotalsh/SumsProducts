94fe115c64da527145486dc312e47b02ed359f28
rotalsh <100392201+rotalsh@users.noreply.github.com>
2023-09-19 13:41:47 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,8 @@ public class Main {
         NumberNumbers sums = new NumberNumbers();   // a NumberNumbers is HashMap<Integer, ArrayList<Integer>>
         NumberNumbers prods = new NumberNumbers();
         int n = 100;      // x + y <= n
-        for(int x = 2; x <= n/2; x++){ 
-            for(int y = x; x + y <= n; y++){ 
+        for (int x = 2; x <= n/2; x++) { 
+            for (int y = x; x + y <= n; y++) { 
                 int sum = x + y;
                 int prod = x * y;
                 sums.addNum(sum, prod);    // for a given sum s = x+y, map all its possible products of x*y
@@ -26,28 +26,28 @@ public class Main {
         System.out.println(pairs);
     }
 
-    // Return a list of SumProductPairs, where 
+    // Returns a list of SumProductPair where the each sum that appears only has one product in its mapped products that appear in validProds, and is paired with this product
     private static ArrayList<SumProductPair> getValidPairs(ArrayList<Integer> validSums,
                                                            ArrayList<Integer> validProds, NumberNumbers sums) {
         ArrayList<SumProductPair> validPairs = new ArrayList<>();
         HashMap<Integer, ArrayList<Integer>> currSums = sums.getNumNums();
 
-        // Returns a list of SumProductPair where the each sum that appears only has one product in its mapped products that appear in validProds, and is paired with this product
-        for(int sum: validSums){
+        // for each sum in validSums
+        for (int sum: validSums) {
             AtomicInteger numOfValidProds = new AtomicInteger(); // track number of validProds
             AtomicInteger correctProd = new AtomicInteger();     // track the most recent product that's contained in validProds
             // for each product mapped to sum
-            for(int prod : currSums.get(sum)){
+            for (int prod : currSums.get(sum)) {
                 if (numOfValidProds.get() > 1)  {
                     break;
-                } else if (validProds.contains(prod)){  // if validProds contains prod set correctProd to prod and increment numOfValidProds
+                } else if (validProds.contains(prod)) {  // if validProds contains prod set correctProd to prod and increment numOfValidProds
                     correctProd.set(prod);
                     numOfValidProds.getAndIncrement();
                 }
             }
             // if numOfValidProds == 1 add new SumProductPair of sum and correctProd
             // numOfValidProds equals 1 if there is only one product in the list of products mapped to sum that's in validProds
-            if(numOfValidProds.get() == 1){
+            if (numOfValidProds.get() == 1) {
                 validPairs.add(new SumProductPair(sum, correctProd.get()));
             }
         }
@@ -63,14 +63,14 @@ public class Main {
         HashMap<Integer, ArrayList<Integer>> currProds = prods.getNumNums();
 
         // for each sum in validSums
-        for(int validSum : validSums){
+        for (int validSum : validSums) {
             // go through its mapped products, and for each product,
-            for(int prod : currSums.get(validSum)){
+            for (int prod : currSums.get(validSum)) {
                 AtomicBoolean canAdd = new AtomicBoolean(true);
                 // go through its mapped sums,
-                for(int checkSum : currProds.get(prod)){
+                for (int checkSum : currProds.get(prod)) {
                     // and if more than one sum in its mapped sums is in validSums,
-                    if(checkSum != validSum && validSums.contains(checkSum)){
+                    if (checkSum != validSum && validSums.contains(checkSum)) {
                         // set canAdd to false and leave loop
                         canAdd.set(false);
                         break;
@@ -78,7 +78,7 @@ public class Main {
                 }
                 // if canAdd is true, add product to validProds
                 // canAdd is true if within the sums mapped to a product, there is only one of these sums in validSums
-                if(canAdd.get()){
+                if (canAdd.get()) {
                     validProds.add(prod);
                 }
             }
@@ -92,9 +92,9 @@ public class Main {
         ArrayList<Integer> singleProducts = new ArrayList<>();
         HashMap<Integer, ArrayList<Integer>> currProds = prods.getNumNums();
         // for each product mapped to sums,
-        for (int prod: currProds.keySet()){
+        for (int prod: currProds.keySet()) {
             // if the product is mapped to only one sum, add it to singleProducts
-            if (currProds.get(prod).size() == 1){
+            if (currProds.get(prod).size() == 1) {
                 singleProducts.add(prod);
             }
         }
@@ -106,12 +106,12 @@ public class Main {
         HashMap<Integer, ArrayList<Integer>> currSums = sums.getNumNums();
         ArrayList<Integer> validSums = new ArrayList<>();
         // for each sum mapped to products, 
-        for(int sum : currSums.keySet()){
+        for (int sum : currSums.keySet()) {
             AtomicBoolean canAdd = new AtomicBoolean(true);
             // go through its list of mapped products
             for (int prod : currSums.get(sum)){
                 // and check if any of them are in prods
-                if(prods.contains(prod)){
+                if (prods.contains(prod)) {
                     // if so, set canAdd to false and leave the loop
                     canAdd.set(false);
                     break;
@@ -119,7 +119,7 @@ public class Main {
             }
             // if canAdd is true, add sum to validSums
             // canAdd is true if every product mapped to a sum is not in the list prods
-            if(canAdd.get()){
+            if (canAdd.get()) {
                 validSums.add(sum);
             }
         }
