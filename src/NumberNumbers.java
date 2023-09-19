5734e59ac9fd060ed37040f459d5418f86aedee2
rotalsh <100392201+rotalsh@users.noreply.github.com>
2023-09-19 13:45:34 -0700

    Update NumberNumbers.java

--- src/NumberNumbers.java
+++ src/NumberNumbers.java
@@ -4,19 +4,24 @@ import java.util.HashMap;
 public class NumberNumbers {
     private HashMap<Integer, ArrayList<Integer>> numNums;
 
+    // EFFECTS: makes new NumberNumbers with numNums an empty hashmap
     public NumberNumbers(){
         this.numNums = new HashMap<>();
     }
 
+    // MODIFIES: this
+    // EFFECTS: adds relNum to the list mapped to mainNum - if no list exists, create the list then add
     public void addNum(int mainNum, int relNum){
         this.numNums.putIfAbsent(mainNum, new ArrayList<>());
         this.numNums.get(mainNum).add(relNum);
     }
 
+    // EFFECTS: return numNums
     public HashMap<Integer, ArrayList<Integer>> getNumNums() {
         return numNums;
     }
 
+    // EFFECTS: returns string representation of NumberNumbers
     @Override
     public String toString(){
         StringBuilder message = new StringBuilder("");
